56707821b6693c7bfe5260cb33fc4b3e875f2b2d
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-08-21 20:38:35 -0500

    Added sendmessage function

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,8 @@
 'use client';
 import { useState } from 'react';
 
+import axios from 'axios';
+
 function Chat() {
 	const [input, setInput] = useState('');
 	const [chatRecord, setChatRecord] = useState([]);
@@ -12,7 +14,35 @@ function Chat() {
 			...prevChatRecord,
 			{ type: 'user', message: input },
 		]);
+		sendMessage(input);
 		setInput('');
+	};
+
+	const sendMessage = (message) => {
+		const url = 'https://api.openai.com/v1/chat/completions ';
+		const headers = {
+			'Content-type': 'application/json',
+			'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+		};
+		const data = {
+			model: 'gpt-3.5-turbo',
+			messages: [{ role: 'user', content: message }],
+		};
+
+		setIsLoading(true);
+
+		axios.post(url, data, { headers: headers }).then((response) => {
+					console.log(response);
+					setChatRecord((prevChatRecord) => [...prevChatRecord, {
+							type: 'bot',
+							message: response.data.choices[0].message.content,
+						},
+					]);
+					setIsLoading(false);
+				}).catch((error) => {
+				setIsLoading(false);
+				console.log(error);
+			});
 	};
 
 	return (
@@ -33,5 +63,7 @@ function Chat() {
 		</>
 	);
 }
+
+
 
 export default Chat;
